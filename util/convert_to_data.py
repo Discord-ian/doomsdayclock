a354946fecaa6557e77ab85d4c73675b74aa2ab6
Discord-ian <aidanbergeman@yahoo.com>
2020-10-16 14:36:52 -0500

    add comments just in case

--- util/convert_to_data.py
+++ util/convert_to_data.py
@@ -1,11 +1,12 @@
 import sys
 import json
 file_to_convert = str(sys.argv[1])
-list_before = open(file_to_convert).read().split("\n")
+list_before = open(file_to_convert).read().split("\n") #split using linebreaks
 years = []
 minutes = []
 combined = {}
-for itemset in list_before:
+# goes through all items, putting them in appropriate lists
+for itemset in list_before: 
     split_object = itemset.split(",")
     year = split_object[0]
     minute = split_object[1]
@@ -14,5 +15,6 @@ for itemset in list_before:
     combined[str(year)] = minute
 with open("exported.json", "w+") as out:
     to_export = {"years": years, "minutes": minutes, "data": combined}
-    json.dump(to_export, out)
+    json.dump(to_export, out) # converts python dict to str
     out.close()
+print("Exported data to exported.json")
